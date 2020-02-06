f79462e9299e93ab70a9f0fb478eb11a7b16690e
ameth <majhdkl@gmail.com>
2020-02-06 16:07:57 +0800

    新增Excel导入导出工具类

---
+++ src/main/java/cn/dyoon/review/common/excel/annotation/ExcelFileDesc.java
@@ -0,0 +1,29 @@
+package cn.dyoon.review.common.excel.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author majhdk
+ * @DESCRIPTION Excel文件描述
+ * @date 2018-12-02
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface ExcelFileDesc {
+
+    /**
+     * 编码格式，暂时未使用，后续完善
+     */
+    String charset() default "utf-8";
+
+    /**
+     * 忽略的行，也是数据开始行
+     */
+    int skipLines() default 0;
+
+}
