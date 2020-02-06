5944d4549791a5e9f8af00378ed9376689b87c3f
ameth <majhdkl@gmail.com>
2020-02-06 14:09:10 +0800

    init

---
+++ src/main/java/cn/dyoon/review/EnterpriseResumptionReviewApplication.java
@@ -0,0 +1,13 @@
+package cn.dyoon.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseResumptionReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseResumptionReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/dyoon/review/EnterpriseResumptionReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dyoon.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseResumptionReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
