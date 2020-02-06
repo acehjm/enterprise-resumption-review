8dbf44c5885452ebec97fb8aab1cb3b7a36ca602
ameth <majhdkl@gmail.com>
2020-02-06 23:29:23 +0800

    更新初始化脚本

---
+++ src/main/java/cn/dyoon/review/manage/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * cn.dyoon.review.manage
+ *
+ * @author majhdk
+ * @date 2020/2/6
+ */
+package cn.dyoon.review.manage;
---
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,83 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for enterprise_info 企业信息
+-- ----------------------------
+DROP TABLE IF EXISTS `enterprise_info`;
+CREATE TABLE `enterprise_info`  (
+  `id` varchar(32) NOT NULL COMMENT '企业ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业名称',
+  `type` tinyint(1) NULL DEFAULT NULL COMMENT '企业类型',
+  `unified_social_credit_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '统一社会信用代码',
+  `street` tinyint(1) NULL DEFAULT NULL COMMENT '所属街道',
+  `transactor_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '填报人姓名',
+  `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '联系电话',
+  `apply_date` datetime(0) NULL DEFAULT NULL COMMENT '申请时间',
+  `review_status` tinyint(1) NULL DEFAULT NULL COMMENT '审核状态',
+  `review_user` varchar(32) NULL DEFAULT NULL COMMENT '审核人',
+  `review_time` datetime(0) NULL DEFAULT NULL COMMENT '审核时间',
+  `review_result` varchar (255) NULL DEFAULT NULL COMMENT '审核结果',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for policy_document 政策附件文档
+-- ----------------------------
+DROP TABLE IF EXISTS `policy_document`;
+CREATE TABLE `policy_document`  (
+  `id` varchar(32) NOT NULL,
+  `file_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文件名称',
+  `file_size` double(11, 1) NULL DEFAULT NULL COMMENT '文件大小',
+  `upload_user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上传人姓名',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文档存放位置',
+  `policy_id` varchar(32) NULL DEFAULT NULL COMMENT '政策信息表ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for policy_info 政策信息
+-- ----------------------------
+DROP TABLE IF EXISTS `policy_info`;
+CREATE TABLE `policy_info`  (
+  `id` varchar(32) NOT NULL COMMENT '政策信息ID',
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `desc` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `release_date` datetime(0) NULL DEFAULT NULL COMMENT '发布时间',
+  `status` tinyint(1) NULL DEFAULT NULL COMMENT '状态（0-未发布，1-发布）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for rework_document 复工附件文档
+-- ----------------------------
+DROP TABLE IF EXISTS `rework_document`;
+CREATE TABLE `rework_document`  (
+  `id` varchar(32) NOT NULL COMMENT '文档ID',
+  `file_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `file_size` double(11, 1) NULL DEFAULT NULL COMMENT '文件大小',
+  `upload_user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上传人',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文档存放位置',
+  `enterprise_id` int(11) NULL DEFAULT NULL COMMENT '企业信息表ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user 用户表
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(32) NOT NULL COMMENT '用户ID',
+  `user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建日期',
+  `user_type` tinyint(1) NULL DEFAULT NULL COMMENT '用户类型',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
