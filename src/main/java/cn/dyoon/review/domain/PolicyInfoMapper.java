86cda2104998a59af22f28c8d50f06f42de3b86e
Leo <754119623@qq.com>
2020-02-07 12:16:58 +0800

    政策文件发布、下载

--- src/main/java/cn/dyoon/review/domain/PolicyInfoMapper.java
+++ src/main/java/cn/dyoon/review/domain/PolicyInfoMapper.java
@@ -1,6 +1,7 @@
 package cn.dyoon.review.domain;
 
 import cn.dyoon.review.domain.entity.PolicyInfoDO;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface PolicyInfoMapper extends BaseMapper<PolicyInfoDO> {
+
+    default int publish(PolicyInfoDO policyInfoDO, String policyId) {
+        return update(policyInfoDO, new QueryWrapper<PolicyInfoDO>().eq("id", policyId));
+    }
 
 }
